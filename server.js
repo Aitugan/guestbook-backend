467809efeed93b5c66294503e7cec8213e769342
Aitugan <mirash2001@gmail.com>
2023-05-06 01:24:00 +0600

    feat: created a server with 2 routes, and mongodb and socket.io connection

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server,{ 
+    cors: {
+      origin: 'http://localhost:3000'
+    }
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+mongoose.connect(
+  'url',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  text: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+app.get('/api/messages', async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/api/messages', async (req, res) => {
+  const message = new Message({
+    name: req.body.name,
+    text: req.body.text,
+  });
+  try {
+    const newMessage = await message.save();
+    console.log("newMessage",newMessage.text);
+    io.emit('newMessage', newMessage);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
